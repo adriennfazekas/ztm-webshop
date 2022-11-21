bc0f1b4cbff9742f088f38dd6d5f6144986d60e2
Fazekas Adrienn <adreeja17@gmail.com>
2022-11-21 23:11:36 +0100

    menu item and directory components

---
+++ crwn-clothing/src/pages/homepage/homepage-component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory-component";
+
+import "./homepage-style.scss";
+
+const HomePage = () => (
+    <div className="homapage">
+        <Directory />
+    </div>
+    );
+
+    export default HomePage;
